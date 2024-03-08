97c5f308ca70dc7c76ca61e79ddd8f07f2b3b281
Reem Rashid <reemsrashid8@gmail.com>
2024-03-07 21:04:49 -0700

    firstphase

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -49,20 +49,16 @@ public class CustomList extends ArrayAdapter<City> {
      * this gets size of the list
      * @return
      */
-    /**
-     * this gets size of the list
-     * @return
-     */
     public int getCount(){
         return cities.size();
     }
     /**
      * this adds a city object to the list
-     *the second phase, you can add the
-     city * @param city
+     *for the first phase it will be
+     empty * @param city
      */
     public void addCity(City city){
-        cities.add(city);
     }
+
 
 }
